33927711d233a58b13c73a52eb19ff7837557b20
Couts <imzachcoutsoutlook.com>
2026-03-17 16:44:33 -0400

    basic auth added

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+
+// basic auth
+
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const auth = request.headers.get('authorization')
+  const validAuth = 'Basic ' + btoa('admin:yourpassword')
+
+  if (auth !== validAuth) {
+    return new NextResponse('Unauthorized', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure"',
+      },
+    })
+  }
+}
+
+export const config = {
+  matcher: '/((?!_next|favicon.ico).*)',
+}
